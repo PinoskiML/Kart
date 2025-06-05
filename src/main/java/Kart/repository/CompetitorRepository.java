0e10968cec433c52200f30fa6f2386fd1be4824c
PinoskiML <felipe.montagne.lascaris@gmail.com>
2025-06-05 11:59:04 +0200

    Final

--- src/main/java/Kart/repository/CompetitorRepository.java
+++ src/main/java/Kart/repository/CompetitorRepository.java
@@ -6,10 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CompetitorRepository extends JpaRepository<Competitor, Integer> {
     List<Competitor> findAllByCompetitorClass(CompetitorClass competitorClass);
 
     List<Competitor> findAllByTotalRacesLessThan(Integer totalRaces);
+
 }
--- src/test/java/Kart/repository/CompetitorRepositoryTest.java
+++ src/test/java/Kart/repository/CompetitorRepositoryTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -24,6 +25,11 @@ class CompetitorRepositoryTest {
 
     }
 
+    @Test
+    public void competitorExists() {
+        Optional<Competitor> optionalCompetitor = competitorRepository.findById(1);
+        assertTrue(optionalCompetitor.isPresent());
+    }
 
 
 }
